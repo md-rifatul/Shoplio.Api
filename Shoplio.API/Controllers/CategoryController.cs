c60cbea59e672415564ddf3519e81637b25c0bd1
Rifatul <rifatul.ism@gmail.com>
2026-04-18 20:30:07 +0600

    Category create complete

---
+++ Shoplio.API/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoplio.Application.DTOs;
+using Shoplio.Application.Interfaces.IServices;
+
+namespace Shoplio.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CategoryCreate([FromBody]CategoryCreateDto dto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var category = await _categoryService.CreateAsync(dto);
+            return Ok(category);
+        }
+
+    }
+}
--- Shoplio.Domain/Entities/Category.cs
+++ Shoplio.Domain/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ namespace Shoplio.Domain.Entities
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Name { get; set; }
         public string? Description { get; set; }
 
         public ICollection<Product>? Products { get; set; }
